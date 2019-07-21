ba49d497ff160f1c8ff7c4daa3b771796c4595c7
Jimmi Setiawan <53130905+MiSetya@users.noreply.github.com>
2019-07-21 19:26:58 +0700

    Add files via upload

---
+++ Aktif.php
@@ -0,0 +1,37 @@
+<?php
+/*
+Cuma Hasil Recode
+*/
+echo "
+
+######################
+# Welcome To my TOOLS #
+#######################
+";
+
+echo "\e[0;36m Tekan Enter untuk Menjalankan Program\n Start : ";
+$gaada = trim(fgets(STDIN));
+$dir = "/data/data/com.termux/files/home/.termux";
+$file_to_write = 'termux.properties';
+$content_to_write = "extra-keys = [['ESC','/','-','HOME','UP','END','PGUP'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT','PGDN']]";
+if( is_dir($dir) === false )
+{
+    mkdir($dir);
+}
+$file = fopen($dir . '/' . $file_to_write,"w");
+fwrite($file, $content_to_write);
+fclose($file);
+include $dir . '/' . $file_to_write;
+system('termux-reload-settings');
+system("clear");
+echo "\e[0;41m  //////////////////////
+ // [+] Berhasil [+] //
+//////////////////////\e[0;36m
+╭┳┳╮
+┃┃┃┃
+┃┗┗┃    Author : MiSetya
+┃┃╭━┃   Team : Termux NooB
+╰┳━┳╯   
+ ┃┈┃    Youtube : MiSetya
+";
+?>
